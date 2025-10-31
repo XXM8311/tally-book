05634fd6b17384d14614e6de1511529c1319bda2
jony <zhangjiawei8311@gmail.com>
2025-10-31 15:18:02 +0800

    Delete unpackage/dist directory

--- unpackage/dist/dev/mp-weixin/api/record.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const api_request = require("./request.js");
-const addRecord = (data) => {
-  return api_request.request.post("record/add", data);
-};
-exports.addRecord = addRecord;
